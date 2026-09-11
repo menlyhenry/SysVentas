d03f3a5199843eb421238e31284792137cdc0468
menlyhenry17 <henrimenly4@gmail.com>
2026-09-11 14:13:12 -0500

    sesion05

---
+++ SysVentas/src/main/java/pe/edu/upeu/sysventas/model/Perfil.java
@@ -0,0 +1,15 @@
+package pe.edu.upeu.sysventas.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Setter
+@Getter
+public class Perfil {
+    Long idPerfil;
+    String nombre;
+    String codigo;
+    List<Acceso> accesos;
+}
